07a7b2a633f78344b1bc5478c48b6819f9ef092e
WesNetmo <netmosfera@gmail.com>
2013-08-09 00:43:26 +0200

    Create ResponsiveAdsense.js

---
+++ ResponsiveAdsense.js
@@ -0,0 +1,32 @@
+var ResponsiveAdsense = (function(){
+  var THIS = this;
+	THIS._banners = [];
+	THIS._t = ['WebkitTransform','MozTransform','OTransform ','msTransform','transform'];
+
+	THIS.resizeHandler = function(){
+		var bc = THIS._banners;
+		for(var x=0;x<bc.length;x++)
+		{
+			var banner = bc[x].getElementsByTagName("ins")[0];
+			if(!banner) throw new Error('The ResponsiveAdsense container should contain the element <ins class="adsbygoogle" ...>');
+			for(var y=0; y<THIS._t.length; y++)
+			{
+				try{
+					banner.style[THIS._t[y]] = 'scaleX(' + (bc[x].clientWidth / banner.offsetWidth) + ') scaleY(' + (bc[x].clientHeight / banner.offsetHeight) + ')';
+					banner.style[THIS._t[y]+'Origin'] = 'top left';
+				} catch(e){};
+			}
+		}
+	};
+
+	return this;
+})();
+
+ResponsiveAdsense.addBanner = function(el){
+	this._banners.push(el);
+};
+
+ResponsiveAdsense.addBanners = function(arrEls){
+	for(var x=0;x<arrEls.length;x++)
+		this.addBanner(arrEls[x]);
+};
